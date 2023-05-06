1e7822fc7d59062084d7c48cea81918b8aa1ac5d
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-05-06 23:54:02 +0100

    updated modals with turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,37 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+Turbo.setConfirmMethod((message, element, button) => {
+  console.log(message, element)
+
+  let confirmText = element.dataset.turboConfirmText
+  let description = element.dataset.turboConfirmDescription || ""
+
+  let dialog = document.getElementById("turbo-confirm")
+  let confirmField = dialog.querySelector("[data-behavior='confirm-text']")
+  let commitButton = dialog.querySelector("button[value='confirm']")
+
+  dialog.querySelector("[data-behavior='title']").textContent = message
+  dialog.querySelector("[data-behavior='description']").innerHTML = description
+  confirmField.value = ""
+
+  if (confirmText) {
+    confirmField.style.display = ""
+    commitButton.disabled = true
+
+    confirmField.addEventListener("input", (event) => {
+      commitButton.disabled = (event.target.value != confirmText)
+    })
+  } else {
+    confirmField.style.display = "none"
+  }
+
+  dialog.showModal()
+
+  return new Promise((resolve, reject) => {
+    dialog.addEventListener("close", () => {
+      resolve(dialog.returnValue == "confirm")
+    }, { once: true })
+  })
+})
