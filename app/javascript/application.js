b2252bca0bc03ba11894d029047a4e2f15e39198
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-05-13 22:11:19 +0100

    real seeds and adjusting modal logic

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,91 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+Turbo.setConfirmMethod((message, element, button) => {
+  console.log(message, element);
+
+  let confirmText = element.dataset.turboConfirmText;
+  let description = element.dataset.turboConfirmDescription || "";
+
+  let dialog = document.getElementById("turbo-confirm");
+  let titleElement = dialog.querySelector(".dialog-title");
+  let descriptionElement = dialog.querySelector(".dialog-description");
+  let confirmField = dialog.querySelector(".confirm-text");
+  let confirmButton = dialog.querySelector(".confirm-button");
+  let cancelButton = dialog.querySelector(".cancel-button");
+
+  if (titleElement) {
+    titleElement.textContent = message;
+  }
+  if (descriptionElement) {
+    descriptionElement.textContent = description;
+  }
+
+  if (confirmField) {
+    confirmField.value = "";
+    confirmField.style.display = confirmText ? "" : "none";
+  }
+
+  if (confirmText && confirmButton) {
+    confirmButton.disabled = true;
+
+    confirmField.addEventListener("input", (event) => {
+      confirmButton.disabled = event.target.value !== confirmText;
+    });
+  }
+
+  dialog.showModal();
+
+  return new Promise((resolve, reject) => {
+    const confirmHandler = () => {
+      dialog.removeEventListener("close", closeHandler);
+      resolve(true);
+    };
+
+    const closeHandler = () => {
+      dialog.removeEventListener("confirm", confirmHandler);
+      reject(false);
+    };
+
+    dialog.addEventListener("confirm", confirmHandler);
+    dialog.addEventListener("close", closeHandler, { once: true });
+
+    cancelButton.addEventListener("click", () => {
+      dialog.close();
+    });
+
+    confirmButton.addEventListener("click", () => {
+      if (confirmField.value === confirmText) {
+        dialog.close();
+        resolve(true);
+
+        // Perform the delete action here
+        // You can make an AJAX request or submit the delete form
+        // Example using AJAX request:
+        fetch(element.action, {
+          method: element.method,
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").getAttribute("content")
+          },
+        })
+        .then(response => {
+          if (response.ok) {
+            // Handle successful deletion
+          } else {
+            // Handle deletion failure
+          }
+        })
+        .catch(error => {
+          // Handle error
+        });
+      } else {
+        // Reject the promise if the confirm text is incorrect
+        reject(false);
+      }
+    });
+  });
+});
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
-require 'faker'
+# require 'faker'
 
-num_films = 10
-num_paragraphs = 3
+Film.destroy_all
 
-num_films.times do
-  # The following code will generate a film title and body
-  title = Faker::Movie.title
-  body = Faker::Lorem.paragraphs(number: num_paragraphs, supplemental: false).join("\n\n")
+films = [
+  { title: 'The Shawshank Redemption', body: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.' },
+  { title: 'The Godfather', body: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'},
+  { title: 'The Dark Knight', body: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.' },
+  { title: 'Eraserhead', body: 'A man tries to survive in an industrial wasteland while caring for his deformed baby.' },
+  { title: 'Blue Velvet', body: 'A young man returns to his hometown and becomes embroiled in a dark underworld of sex and violence.' },
+  { title: 'Mulholland Drive', body: 'A woman with amnesia tries to piece together her past while navigating the seedy underbelly of Hollywood.' },
+  { title: 'Lost Highway', body: 'A jazz musician is accused of murdering his wife and goes on the run, only to find himself in a surreal alternate reality.' },
+  { title: 'Inland Empire', body: 'An actress becomes embroiled in a mysterious and terrifying world while filming a movie.' },
+  { title: 'Donnie Darko', body: 'A troubled teenager is plagued by visions of a giant rabbit that may or may not be real.' },
+  { title: 'The Cell', body: 'An FBI agent enters the mind of a comatose serial killer to find his latest victim.' },
+  { title: 'The Grandmother', body: 'A young boy grows a grandmother from a seed and discovers the horrors of his own creation.' }
+]
 
-  # This will create a new film in the database
-  Film.create(title: title, body: body)
+films.each do |film|
+  Film.create(film)
 end
 
+# num_films = 10
+# num_paragraphs = 3
+
+# num_films.times do
+#   # The following code will generate a film title and body
+#   title = Faker::Movie.title
+#   body = Faker::Lorem.paragraphs(number: num_paragraphs, supplemental: false).join("\n\n")
+
+#   # This will create a new film in the database
+#   Film.create(title: title, body: body)
+# end
+
 # Message below is showing the creation of films
-p "Created #{num_films} films with #{num_paragraphs} paragraphs each."
+p "Added #{'11'} films to the website with #{'description'} paragraphs if located."
