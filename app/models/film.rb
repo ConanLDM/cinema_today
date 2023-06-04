72cb6fe7f97dda225ca0b4e96e4a9c54253d3013
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-06-04 17:31:45 +0100

    reviews setup including MVC for reviews and routing adjustments

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,4 @@
 class Film < ApplicationRecord
   has_one_attached :photo
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   mount Motor::Admin => '/motor_admin'
-  resources :films
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :films do
+    resources :reviews, only: [:index, :new, :create]
+  end
+
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+
   root to: "films#index"
-  resources :films, except: :index
   resources :users, only: [:index]
 end
